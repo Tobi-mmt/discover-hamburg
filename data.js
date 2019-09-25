7eddcb9c5dc1ded8f4b622bfb25440546935c22e
Tobias <tobi.l@posteo.at>
2019-09-25 15:09:54 +0200

    initial commit

---
+++ data.js
@@ -0,0 +1,14 @@
+const DATA = [{
+  name: "Elphi Aussichtsplattform",
+  image: "",
+  description: "Auf der Aussichtsplattform hat man einen schönen Blick über den ganzen Hafen",
+  locationLink: "https://g.page/elbphilharmonie-hh?share",
+  websiteLink: "https://www.elbphilharmonie.de/de/plaza",
+  duration: 40,
+  cost: 0
+},
+{
+
+}]
+
+export default DATA
