744979e087a3e466f357e23884f430ac596b5266
Tobias <tobi.l@posteo.at>
2019-09-25 19:40:57 +0200

    feat: Add more places

--- data.js
+++ data.js
@@ -5,21 +5,98 @@ const DATA = [{
   description: "Auf der Aussichtsplattform hat man einen schönen Blick über den ganzen Hafen.",
   locationLink: "https://g.page/elbphilharmonie-hh?share",
   websiteLink: "https://www.elbphilharmonie.de/de/plaza",
-  duration: 40,
+  duration: "40 m",
   cost: 0,
   weather: "sunny"
 },
 {
   id: 2,
-  name: "Elphi Aussichtsplattform",
-  image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Elbphilharmonie%2C_Hamburg.jpg/1024px-Elbphilharmonie%2C_Hamburg.jpg",
-  description: "Auf der Aussichtsplattform hat man einen schönen Blick über den ganzen Hafen.",
-  locationLink: "https://g.page/elbphilharmonie-hh?share",
-  websiteLink: "https://www.elbphilharmonie.de/de/plaza",
-  duration: 40,
+  name: "Miniatur Wunderland",
+  image: "https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Funterkunft-reise.com%2Fwp-content%2Fuploads%2F2014%2F07%2FMiniatur-Wunderland.jpg",
+  description: "Sollte das Wetter draußen doof sein, könnt ihr hier drin die ganze Welt im Trockenen sehen.",
+  locationLink: "https://goo.gl/maps/TjMGdYFfSsm49EzV9",
+  websiteLink: "https://www.miniatur-wunderland.de/",
+  duration: "2 h",
+  cost: 15,
+  weather: "cloudy"
+},
+{
+  id: 3,
+  name: "Elbtunnel nach Steinwerder",
+  image: "https://upload.wikimedia.org/wikipedia/commons/e/eb/Hamburg%2C_Landungsbr%C3%BCcken_--_2016_--_3110.jpg",
+  description: "Am Besten bei Sonnenuntergang! Durch den Elbtunnel durch seht ihr den Hafen und Schiffe von der anderen Seite und die Abendsonne in der Elphi spiegeln.",
+  locationLink: "https://goo.gl/maps/JKztpCZb99pczG8X7",
+  websiteLink: "",
+  duration: "1 h",
+  cost: 0,
+  weather: "sunny"
+},
+{
+  id: 4,
+  name: "Bonscheladen",
+  image: "https://www.hamburg-magazin.de/public/_processed_/csm_bonscheladen_cbf7586fc7.jpg",
+  description: "Hier werden Bonbons gemacht! Und zwar von Hand und mit sehr coolen Motiven IM Bonbon! Das ist auch ein gutes Mitbringsel für die Süßen Zuhause!",
+  locationLink: "https://goo.gl/maps/4R5DfLTdjGEbkLHS9",
+  websiteLink: "http://www.bonscheladen.de/",
+  duration: "15 m",
   cost: 0,
   weather: "cloudy"
-
-}]
+},
+{
+  id: 5,
+  name: "Ottensen erkunden",
+  image: "http://www.altona.info/wp-content/uploads/2010/04/Ottensen_700_Veranstaltungen.jpg",
+  description: "Die Bahrenfelder Str. und die Ottensen Hauptstr. sind zur Zeit Autofrei. Hier haben sich Cafes und Läden breit gemacht, die erkundet werden können.",
+  locationLink: "https://goo.gl/maps/Z6qjRKWT78pSXmkj7",
+  websiteLink: "https://ottensenmachtplatz.de/",
+  duration: "2 h",
+  cost: 0,
+  weather: "cloudy"
+},
+{
+  id: 6,
+  name: "Einkaufen auf der Mönke",
+  image: "https://cdn01.eavar.com/2017/2/6ad3452d-e537-4f70-9f3f-fa6652fdce91.jpg",
+  description: "Wenn ihr irgend etwas shoppen wollt, seit ihr hier richtig. Die Königsstraße aus Stuttgart in klein.",
+  locationLink: "https://goo.gl/maps/i6c7JzZ4xV4ZSbu18",
+  websiteLink: "",
+  duration: "3 h",
+  cost: 0,
+  weather: "cloudy"
+},
+{
+  id: 7,
+  name: "Ohlsdorfer Friedhof",
+  image: "http://img.fotocommunity.com/mausoleum-auf-dem-friedhof-ohlsdorf-a597cf6e-d773-4b4a-96c6-68f913636557.jpg?height=500",
+  description: "Klingt komisch, ist aber ein gros angelegter Park mit Friedhof. Hier gibt es 12 schöne Kapellen, jede Menge Park und auch zauberhafte Gräber.",
+  locationLink: "https://goo.gl/maps/9CZ7wepVMnaSfKsS7",
+  websiteLink: "",
+  duration: "2 h",
+  cost: 0,
+  weather: "sunny"
+},
+{
+  id: 7,
+  name: "Holdhusenbad",
+  image: "https://www.hamburg-magazin.de/public/_processed_/csm_holthusenbad_eppendorf_3dbfa3645c.jpg",
+  description: "Zwar gibt es in Hamburg keine Quellen wie in Stuttgart, aber auch hier kann man sich in Thermen entspannen. Massagen und Saunen gibts natürlich auch.",
+  locationLink: "https://goo.gl/maps/tMCMUHyP1oENvPfa9",
+  websiteLink: "https://www.baederland.de/bad/holthusenbad.html",
+  duration: "3 h",
+  cost: 19,
+  weather: "cloudy"
+},
+{
+  id: 7,
+  name: "",
+  image: "",
+  description: "",
+  locationLink: "",
+  websiteLink: "",
+  duration: "",
+  cost: 0,
+  weather: "cloudy"
+},
+]
 
 export default DATA
